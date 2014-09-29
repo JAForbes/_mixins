cd73909797e63976e98df637c98db6e5c7dbc7f6
James <james.a.forbes@gmail.com>
2014-09-29 10:54:13 +1000

    More nodey

--- underscore-mixins.js
+++ underscore-mixins.js
@@ -1,13 +1,21 @@
-_.mixin({
-  	distance: function(p1, p2) {
-		var x = Math.pow(p2.x - p1.x,2);
-		var y = Math.pow(p2.y - p1.y,2);
-		return Math.sqrt(x + y);
-	},
-	translate: function(pos,callback){
-	  con.save();
-	  con.translate(pos.x,pos.y)
-	  callback()
-	  con.restore();
-	}
-})
+/* jshint asi:true */
+
+var mixins = {
+    distance: function(p1, p2) {
+    var x = Math.pow(p2.x - p1.x,2);
+    var y = Math.pow(p2.y - p1.y,2);
+    return Math.sqrt(x + y);
+  },
+  translate: function(pos,callback){
+    con.save();
+    con.translate(pos.x,pos.y)
+    callback()
+    con.restore();
+  }
+}
+
+if(typeof require != 'undefined'){
+    module.exports = mixins;
+} else {
+    _.mixin(mixins);
+}
